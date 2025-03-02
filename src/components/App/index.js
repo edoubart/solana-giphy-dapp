35343525e3e315653d6140816062329aa9f52ad0
edoubart <edouard.barthelemy@gmail.com>
2025-03-02 14:47:49 +0100

    Cleanup and add boilerplate.

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+// Styles
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">
+            View your GIF collection in the metaverse ✨
+          </p>
+        </div>
+        <div className="footer-container">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
